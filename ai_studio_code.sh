0b8fcc76e9b9df12d2f40536777782a14f090a1a
Admin <admin@example.com>
2025-11-22 21:29:05 +0800

    Fix build dependencies and cleanup html

--- ai_studio_code.sh
+++ ai_studio_code.sh
@@ -1,4 +1,3 @@
-# 把下面的地址换成你自己的 GitHub 仓库地址
-git remote add origin https://github.com/760427741mh-cpu/GlacialBreathApp.git
-git branch -M main
-git push -u origin main
+git add .
+git commit -m "Fix build dependencies and cleanup html"
+git push
